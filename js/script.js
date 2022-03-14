159988ba36887e1a81f0dc090b1e087234db9a27
Rita Shih _ FJU <55233170+rItashih-lol@users.noreply.github.com>
2022-03-14 10:03:27 +0800

    20220314-文馨(部分管理者頁面)

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(function () {
+  $(".checkAll").click(function () {
+    var isCheck = $(this).prop("checked");
+    $(this)
+      .closest(".checkAllWrap")
+      .find("input[name='sort']")
+      .prop("checked", isCheck);
+  });
+
+  $(".checkAllWrap input[name='sort']").click(function () {
+    var el_checkAll = $(this).closest(".checkAllWrap").find(".checkAll");
+
+    var checkLength = $(this)
+      .closest(".checkAllWrap")
+      .find("input[name='sort']:checked").length; // 每區塊目前被V的有幾個
+
+    var inputLength = $(this)
+      .closest(".checkAllWrap")
+      .find("input[name='sort']").length; // 每區塊的核取框有多少
+
+    if (!$(this).prop("checked")) {
+      el_checkAll.prop("checked", false);
+    } else {
+      if (checkLength == inputLength) {
+        el_checkAll.prop("checked", true);
+      }
+    }
+  });
+});
